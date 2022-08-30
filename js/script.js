4a1a4d225c97f7968d8f3f33fb3169cd9dc4e814
Ivan <Priest.ivan14@gmail.com>
2022-08-30 13:52:36 +0300

    Разобрал все функции из единого файла script.js на отдельные модули. Подключил их всех и собрал с помощью webpack

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-window.addEventListener('DOMContentLoaded', function ()
-{
+window.addEventListener('DOMContentLoaded', function () {
 
 'use strict';
 
-let tab = document.querySelectorAll('.info-header-tab'),
-    info = document.querySelector('.info-header'),
-    tabContent = document.querySelectorAll('.info-tabcontent'),
-    infoAll = document.querySelector('.info'),
+let infoAll = document.querySelector('.info'),
     timerAll = document.querySelector('.timer'),
-    morebtn = document.querySelector('.more'),
     contentAll = document.querySelector('.content'),
     photoSlider = document.querySelector('#photo'),
     priceCounter = document.querySelector('#price'),
@@ -16,402 +11,99 @@ let tab = document.querySelectorAll('.info-header-tab'),
     photoBTN = document.querySelector('.photo-btn'),
     aboutBTN = document.querySelector('.about-btn'),
     priceBTN = document.querySelector('.price-btn'),
-    contactsBTN = document.querySelector('.contacts-btn'),
-    more = document.querySelectorAll('.description-btn, .more'),
-    overlay = document.querySelector('.overlay'),
-    close = document.querySelector('.popup-close');
+    morebtn = document.querySelector('.more'),
+    contactsBTN = document.querySelector('.contacts-btn');
 
-    function hideTabContent (a) 
-    {
-        for( let i = a; i<tabContent.length; i++)
-        {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-            contentAll.style.minHeight = '1000px';
-        }
-    };
 
-    hideTabContent(1);
+    let tabs = require ('/js/tabs.js');
+    let timer = require ('/js/timer.js');
+    let modal = require ('/js/modal.js');
+    let postPhp = require ('/js/postPhp.js');
+    let sliderPhoto = require ('/js/sliderPhoto.js');
+    let calculator = require ('/js/calculator.js');
 
-    function showTabContent(b)
-    {
-        if ( tabContent[b].classList.contains('hide'))
-        {
-        tabContent[b].classList.remove('hide');
-        tabContent[b].classList.add('show');
-        }
-    };
 
-    function hideContent()
+    function hideAllContent()
     {
         photoSlider.style.display = 'none';
         priceCounter.style.display = 'none';
         contacts.style.display = 'none';
+        contentAll.style.minHeight = '1000px';
         window.scrollTo(0,110);
     };
 
-    hideContent();
+    hideAllContent();
 
 
-        info.addEventListener('click', function(event)
-        {
-        let target = event.target;
-
-        if ( target && target.classList.contains ('info-header-tab'))
-        {
-            for ( let i=0; i<tab.length; i++)
-            {
-                if (target == tab[i])
-                    {
-                        hideTabContent(0);
-                        showTabContent(i);
-                        break;
-                    }
-            }
-        }
-});
- 
-        photoBTN.addEventListener('click', function()
-        {
-            {
-                photoSlider.style.display = 'block';
-                photoSlider.style.marginTop = '0px';
-                priceCounter.style.display = 'none';
-                contacts.style.display = 'none';
-                infoAll.style.display = 'none';
-                timerAll.style.display = 'none';
-                morebtn.style.display = 'none';
-                contentAll.style.minHeight = '1000px';
-                window.scrollTo(0,550);
-            }
-        });
-
-        aboutBTN.addEventListener('click', function()
-        {
-            {
-                photoSlider.style.display = 'none';
-                priceCounter.style.display = 'none';
-                contacts.style.display = 'none';
-                infoAll.style.display = 'block';
-                timerAll.style.display = 'none';
-                morebtn.style.display = 'none';
-                contentAll.style.minHeight = '1000px';
-                window.scrollTo(0,110);
-
-            }
-        });
-
-        priceBTN.addEventListener('click', function()
-        {
-            {
-                photoSlider.style.display = 'none';
-                priceCounter.style.display = 'block';
-                contacts.style.display = 'none';
-                infoAll.style.display = 'none';
-                timerAll.style.display = 'block';
-                morebtn.style.display = 'block';
-                contentAll.style.minHeight = '1000px';
-                timerAll.style.marginTop = '0px';
-                window.scrollTo(0,550);
-
-            }
-        });
-
-        contactsBTN.addEventListener('click', function()
-        {
-            {
-                photoSlider.style.display = 'none';
-                priceCounter.style.display = 'none';
-                contacts.style.display = 'block';
-                infoAll.style.display = 'none';
-                timerAll.style.display = 'none';
-                morebtn.style.display = 'none';
-                contentAll.style.minHeight = '1000px';
-                contacts.style.width = '100%';
-                contacts.style.marginTop = '0px';
-                window.scrollTo(0,700);
-
-            }
-        });
-
-        let deadline = '2022-07-05';
-
-        function getTimeRemaining(endtime)
+    photoBTN.addEventListener('click', function()
     {
-        let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)));
-        
-        return {
-            'total': t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    };
-
-    function setClock(id, endtime)
-    {
-    let timer = document.getElementById(id),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds'),
-        timeInterval = setInterval(updateClock, 1000);
-
-    function updateClock()
-    {
-        let t = getTimeRemaining(endtime);
-
-        function addZero(num)
         {
-        if( num<=9 )
-        {
-            return '0' + num;
-        }
-        else return num;
-        };
-
-        hours.textContent = addZero(t.hours);
-        minutes.textContent = addZero(t.minutes);
-        seconds.textContent = addZero(t.seconds);
-
-        if (t.total <= 0)
-        {
-            clearInterval(timeInterval);
-            hours.textContent = '00';
-            minutes.textContent = '00';
-            seconds.textContent = '00';
-        };
-
-    };
-};
-
-setClock('timer', deadline);
-
-// more.addEventListener('click', function(){
-//     overlay.style.display = 'block';
-//     this.classList.add('more-splash');
-//     document.body.style.overflow = 'hidden';
-// });
-
-// close.addEventListener('click', function(){
-//     overlay.style.display = 'none';
-//     more.classList.remove('more-splash');
-//     document.body.style.overflow = '';
-// });
-
-
-more.forEach(function(btn) {
-    btn.addEventListener('click', function() {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';   
-    })
-});
-
-close.addEventListener('click', function() {
-    overlay.style.display = 'none';
-    document.body.style.overflow = '';
-    more.forEach(function(btn) {
-        btn.classList.remove('more-splash');
-    })
-});
-
-let message ={
-    loading: 'Загрузка...',
-    success: 'Спасибо! Скоро мы с вами свяжемся!',
-    failure: 'Что-то пошло не так...'
-};
-
-// В блоке Узнать Больше и Узнать Подробнее
-
-let form = document.querySelector('.main-form'),
-    input = form.getElementsByTagName('input'),
-    statusMessage = document.createElement('div');
-
-    statusMessage.classList.add ('status');
-    form.addEventListener('submit', function (event){
-    event.preventDefault();
-    form.appendChild(statusMessage);
-    let formData = new FormData(form);
-
-function postData (data) {
-return new Promise (function(resolve,reject){
-    let request = new XMLHttpRequest();
-    request.open ('POST', 'server.php');
-    request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
-    request.onreadystatechange = function () {
-        if (request.readyState < 4) {
-            resolve()
-        } else if (request.readyState === 4 && request.status == 200) {
-            resolve()
-        } else {
-            reject()
-        }
-    }
-    request.send (data);
-})
-}
-
-function clearInput(){
-    for( let i=0; i<input.length; i++) {
-        input [i].value = '';
-    }
-}
-
-postData(formData)
-.then ( () => statusMessage.innerHTML = message.loading)
-.then ( () => statusMessage.innerHTML = message.success)
-.catch ( () => statusMessage.innerHTML = message.failure)
-.then (clearInput)
-
-});
-
-
-// В блоке контактов
-
-let formContact = document.querySelector("#form"),
-    inputContact = formContact.getElementsByTagName('input'),
-    messageContact = document.createElement('div');
-    messageContact.classList.add('status');
-    messageContact.style.color = "white";
-    messageContact.style.marginTop = "15px";
-
-    formContact.addEventListener('submit', function(event){
-    event.preventDefault();
-    formContact.appendChild(messageContact);
-    let formData = new FormData(formContact);
-
-    function postData (data) {
-        return new Promise (function(resolve,reject){
-            let request = new XMLHttpRequest();
-            request.open ('POST', 'server.php');
-            request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
-            request.onreadystatechange = function () {
-                if (request.readyState < 4) {
-                    resolve()
-                } else if (request.readyState === 4 && request.status == 200) {
-                    resolve()
-                } else {
-                    reject()
-                }
-            }
-            request.send (data);
-        })
-        }
-
-        function clearInput(){
-            for( let i=0; i<inputContact.length; i++) {
-                inputContact [i].value = '';
-            }
-        }
-
-        postData(formData)
-        .then ( () => messageContact.innerHTML = message.loading)
-        .then ( () => messageContact.innerHTML = message.success)
-        .catch ( () => messageContact.innerHTML = message.failure)
-        .then (clearInput)
-
-});
-
-
-// Слайдер фото (по стрелкам и точкам)
-
-let slideIndex = 1,
-slides = document.querySelectorAll ('.slider-item'),
-prev = document.querySelector('.prev'),
-next = document.querySelector('.next'),
-dotsWrap = document.querySelector('.slider-dots'),
-dots = document.querySelectorAll('.dot');
-
-
-
-// навигация по стрелкам побокам фото
-showSlides(slideIndex);
-
-function showSlides(n){
-
-    if(n>slides.length){
-        slideIndex =1;
-    }
-
-    if(n<1){
-        slideIndex = slides.length;
-    }
-
-    slides.forEach( (item) => item.style.display='none');
-    dots.forEach( (item) => item.classList.remove ('dot-active'));
-    slides [slideIndex -1].style.display = 'block';
-    dots [slideIndex -1].classList.add ('dot-active');
-    
-    }
-
-    function plusSlides (n) {
-        showSlides(slideIndex +=n);
-    }
-
-    function currentSlide (n) {
-        showSlides(slideIndex = n);
-    }
-
-    prev.addEventListener('click', function(){
-        plusSlides (-1);
-    });
-
-    next.addEventListener('click', function(){
-        plusSlides(1);
-    });
-
-    // навигация по точкам
-
-    dotsWrap.addEventListener('click', function (event){
-        for (let i=0; i<dots.length+1; i++){
-            if (event.target.classList.contains ('dot') && event.target == dots [i-1]) {
-                currentSlide(i);
-            }
+            photoSlider.style.display = 'block';
+            photoSlider.style.marginTop = '0px';
+            priceCounter.style.display = 'none';
+            contacts.style.display = 'none';
+            infoAll.style.display = 'none';
+            timerAll.style.display = 'none';
+            morebtn.style.display = 'none';
+            contentAll.style.minHeight = '1000px';
+            window.scrollTo(0,550);
         }
     });
 
-    // калькулятор расчета стоимости путевки
+    aboutBTN.addEventListener('click', function()
+    {
+        {
+            photoSlider.style.display = 'none';
+            priceCounter.style.display = 'none';
+            contacts.style.display = 'none';
+            infoAll.style.display = 'block';
+            timerAll.style.display = 'none';
+            morebtn.style.display = 'none';
+            contentAll.style.minHeight = '1000px';
+            window.scrollTo(0,110);
 
-    let persons = document.querySelectorAll('.counter-block-input')[0],
-        restDays = document.querySelectorAll('.counter-block-input')[1],
-        place = document.getElementById('select'),
-        totalValue = document.getElementById('total'),
-        personsSum=0,
-        daysSum=0,
-        total=0;
-
-        totalValue.innerHTML=0;
-
-        persons.addEventListener('change', function () {
-            personsSum = +this.value;
-        total = (daysSum + personsSum) * 4000;
-        if (restDays.value == '' || persons.value == '') {
-            totalValue.innerHTML = 0;
-        } else {
-            totalValue.innerHTML = total;
         }
-        });
+    });
 
-        restDays.addEventListener('change', function () {
-            daysSum = +this.value;
-        total = (daysSum + personsSum) * 4000;
-        if (restDays.value == '' || persons.value == '') {
-            totalValue.innerHTML = 0;
-        } else {
-            totalValue.innerHTML = total;
+    priceBTN.addEventListener('click', function()
+    {
+        {
+            photoSlider.style.display = 'none';
+            priceCounter.style.display = 'block';
+            contacts.style.display = 'none';
+            infoAll.style.display = 'none';
+            timerAll.style.display = 'block';
+            morebtn.style.display = 'block';
+            contentAll.style.minHeight = '1000px';
+            timerAll.style.marginTop = '0px';
+            window.scrollTo(0,550);
+
         }
-        });
+    });
 
-        place.addEventListener ('change', function () {
-            if (restDays.value == '' || persons.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                let a = total;
-                totalValue.innerHTML = a * this.options [this.selectedIndex].value; 
-            }
-        });
+    contactsBTN.addEventListener('click', function()
+    {
+        {
+            photoSlider.style.display = 'none';
+            priceCounter.style.display = 'none';
+            contacts.style.display = 'block';
+            infoAll.style.display = 'none';
+            timerAll.style.display = 'none';
+            morebtn.style.display = 'none';
+            contentAll.style.minHeight = '1000px';
+            contacts.style.width = '100%';
+            contacts.style.marginTop = '0px';
+            window.scrollTo(0,700);
+
+        }
+    });
+
+
+    tabs();
+    timer();
+    modal();
+    postPhp();
+    sliderPhoto();
+    calculator();
 
 });
